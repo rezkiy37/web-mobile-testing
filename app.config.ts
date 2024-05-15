6760338a5af37c7a267649be12e2e51c495c52f2
Mykhailo Kravchenko <rezkiy37@gmail.com>
2024-05-15 17:08:11 +0200

    init

---
+++ app.config.ts
@@ -0,0 +1,9 @@
+import "ts-node/register"; // Add this to import TypeScript files
+import { ExpoConfig } from "expo/config";
+
+const config: ExpoConfig = {
+  name: "web-mobile-testing",
+  slug: "web-mobile-testing",
+};
+
+export default config;
